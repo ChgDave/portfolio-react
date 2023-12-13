5341b444564655ebfa30e4f748f427bb4fdc65ad
ChgDave <chgdave@gmail.com>
2023-12-12 22:16:22 -0600

    added github link to image

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,31 +5,112 @@ import PigGame from "../../assets/images/piggame.png";
 import TechBlog from "../../assets/images/techblog.png";
 import WeatherBoard from "../../assets/images/weatherdashboard.png";
 import WorkDayScheduler from "../../assets/images/workdayscheduler.png";
-
+import { FaGithub } from "react-icons/fa";
+import { useState } from "react";
 export default function Portfolio() {
+  const [isHovered, setIsHovered] = useState(false);
+  const mouseEnter = () => {
+    setIsHovered(true);
+  };
+  const mouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <div className={style.body}>
       <div className={style.title}>
         <h1>Portfolio</h1>
       </div>
       <div className={style["project-container"]}>
-        <div>
-          <img src={DnD1} />
+        <div
+          onMouseEnter={mouseEnter}
+          onMouseLeave={mouseLeave}
+          className={style["project-card"]}
+        >
+          <img src={DnD1} alt="Project Image" />
+          {isHovered && (
+            <a
+              href="https://github.com/ChgDave/DnD-Character-Generator"
+              className={style.github}
+            >
+              <FaGithub style={{ color: "lightblue" }} size={40} />
+            </a>
+          )}
         </div>
-        <div>
-          <img src={ArtBlockBreaker} />
+        <div
+          onMouseEnter={mouseEnter}
+          onMouseLeave={mouseLeave}
+          className={style["project-card"]}
+        >
+          <img src={ArtBlockBreaker} alt="Project Image" />
+          {isHovered && (
+            <a
+              href="https://github.com/ChgDave/art-block-breaker"
+              className={style.github}
+            >
+              <FaGithub style={{ color: "lightblue" }} size={40} />
+            </a>
+          )}
         </div>
-        <div>
-          <img src={PigGame} />
+        <div
+          onMouseEnter={mouseEnter}
+          onMouseLeave={mouseLeave}
+          className={style["project-card"]}
+        >
+          <img src={PigGame} alt="Project Image" />
+          {isHovered && (
+            <a
+              href="https://github.com/ChgDave/Pig-Game"
+              className={style.github}
+            >
+              <FaGithub style={{ color: "lightblue" }} size={40} />
+            </a>
+          )}
         </div>
-        <div>
-          <img src={TechBlog} />
+        <div
+          onMouseEnter={mouseEnter}
+          onMouseLeave={mouseLeave}
+          className={style["project-card"]}
+        >
+          <img src={TechBlog} alt="Project Image" />
+          {isHovered && (
+            <a
+              href="https://github.com/ChgDave/CMS-Blog"
+              className={style.github}
+            >
+              <FaGithub style={{ color: "lightblue" }} size={40} />
+            </a>
+          )}
         </div>
-        <div>
-          <img src={WeatherBoard} />
+        <div
+          onMouseEnter={mouseEnter}
+          onMouseLeave={mouseLeave}
+          className={style["project-card"]}
+        >
+          <img src={WeatherBoard} alt="Project Image" />
+          {isHovered && (
+            <a
+              href="https://github.com/ChgDave/Weather-Dashboard"
+              className={style.github}
+            >
+              <FaGithub style={{ color: "lightblue" }} size={40} />
+            </a>
+          )}
         </div>
-        <div>
-          <img src={WorkDayScheduler} />
+        <div
+          onMouseEnter={mouseEnter}
+          onMouseLeave={mouseLeave}
+          className={style["project-card"]}
+        >
+          <img src={WorkDayScheduler} alt="Project Image" />
+          {isHovered && (
+            <a
+              href="https://github.com/ChgDave/Work-Day-Scheduler"
+              className={style.github}
+            >
+              <FaGithub style={{ color: "lightblue" }} size={40} />
+            </a>
+          )}
         </div>
       </div>
     </div>
